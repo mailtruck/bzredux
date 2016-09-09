bdb4dcc52162f2b606f8e91c213b10db8a0ed2ce
Brian Danowski <briandanowski@gmail.com>
2016-09-09 18:25:55 -0400

    fixed orderValidator tests

--- src/utils/orderValidator.spec.js
+++ src/utils/orderValidator.spec.js
@@ -5,13 +5,13 @@ import * as types from '../actions/types'
 describe('order validaton function checks SEND_ORDER  action against portfolio state for validity - returns SEND_ORDER if valid or SET_MESSAGE if invalid, and returns any non SEND_ORDER action passed to it as-is', () => {
   it('should return a non SEND_ORDER action that it is passed ', () => {
     const portfolio = {
-      cash: 100000
+      cash: 100000,
       positions: [{
         name:"Apple",
         symbol:"AAPL",
         purchasePrice:123,
         quantity:100
-      }]
+      }]}
       const action = {
         type: types.SET_QUOTE,
         quote: {
@@ -25,10 +25,10 @@ describe('order validaton function checks SEND_ORDER  action against portfolio s
     expect(orderValidator(portfolio, action)).toEqual(action)
   })
 
-  it('should return a SEND_ORDER action with order.type === buy if portfolio has enough cash to pay for the order' () => {
+  it('should return a SEND_ORDER action with order.type === buy if portfolio has enough cash to pay for the order', () => {
     const portfolio = {
       cash: 100000
-      }]
+      }
       const action = {
         type: types.SEND_ORDER,
         order: {
@@ -39,6 +39,7 @@ describe('order validaton function checks SEND_ORDER  action against portfolio s
           askPrice:110,
           quantity:101
         }
+      }
       const action2 = {
         type: types.SEND_ORDER,
         order: {
@@ -70,6 +71,7 @@ describe('order validaton function checks SEND_ORDER  action against portfolio s
           askPrice:110,
           quantity:100
         }
+      }
       const action2 = {
         type: types.SEND_ORDER,
         order: {
@@ -105,6 +107,7 @@ describe('order validaton function checks SEND_ORDER  action against portfolio s
         askPrice:110,
         quantity:101
       }
+    }
     const action2 = {
       type: types.SEND_ORDER,
       order: {
@@ -141,6 +144,7 @@ describe('order validaton function checks SEND_ORDER  action against portfolio s
         askPrice:110,
         quantity:101
       }
+    }
     const action2 = {
       type: types.SEND_ORDER,
       order: {
@@ -158,4 +162,4 @@ describe('order validaton function checks SEND_ORDER  action against portfolio s
     expect(orderValidator(portfolio, action2).type).toEqual(types.SET_MESSAGE)
 
   })
-})const
+})
