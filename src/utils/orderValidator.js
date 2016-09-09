d00f3c75420d62f1c0e0b3e18b588d10bc8ae9fe
Brian Danowski <briandanowski@gmail.com>
2016-09-09 19:21:26 -0400

    orderValidator function

--- src/utils/orderValidator.js
+++ src/utils/orderValidator.js
@@ -1,16 +1,44 @@
-import { SEND_ORDER } from '../actions/types'
-import { BUY, SELL } from '../constants'
+import * as types from '../actions/types'
+import { BUY, SELL, ALERT } from '../constants'
+import findIndex from 'lodash/findIndex'
+import { v4 } from 'node-uuid'
 
-export default function orderValidator(state, action){
-  if (action.type !== SEND_ORDER) {
+export default function orderValidator(portfolio, action){
+  if (action.type !== types.SEND_ORDER) {
     return action
   }
+  const { symbol, askPrice, quantity } = action.order
 
   switch (action.order.type) {
     case BUY:
+      if (portfolio.cash < askPrice * quantity) {
+        return {
+          type: types.SET_MESSAGE,
+          message: {
+            type: ALERT,
+            id: v4(),
+            text: 'You do not have enough cash for that order'
+          }
+        }
+      } else {
+        return action
+      }
       return action
     case SELL:
-      return action
+      const index = findIndex(portfolio.positions, {symbol})
+
+      if (index === -1 || portfolio.positions[index].quantity < quantity) {
+        return {
+          type: types.SET_MESSAGE,
+          message: {
+            type: ALERT,
+            id: v4(),
+            text: `You do not own enough shares of ${symbol} for that order`
+          }
+        }
+      } else {
+        return action
+      }
     default:
       return action
   }
