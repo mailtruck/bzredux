d6be9529d0725740b17407199da87f078c1aa819
Brian Danowski <briandanowski@gmail.com>
2016-09-14 14:27:37 -0400

    invalid ticker symbol searches now message alerting user

--- src/actions/quoteActions.js
+++ src/actions/quoteActions.js
@@ -1,11 +1,17 @@
 import axios from 'axios'
+import { setMessage } from './messageActions'
 import { SET_QUOTE } from './types'
+import { ALERT } from '../constants'
+
 
 export function getQuote(symbol) {
   return dispatch => {
     return axios.get('/api/'+ symbol).then(res=> {
       if (res.data.error){
-        alert()
+        dispatch(setMessage({
+          type: ALERT,
+          text: 'Invalid ticker symbol'
+        }))
       } else {
         dispatch(setQuote(res.data))
       }
